ce51e6fa53547092f8ea797ae44dfbe125625e78
francisco <fr.betancourt@live.com>
2022-01-25 19:22:57 -0300

    Created TokenFarm contract. Added stakeTokens function definition.

---
+++ contracts/TokenFarm.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TokenFarm {
+    // stakeTokens
+    // unStakeTokens
+    // issueTokens - rewards for staking
+    // addAllowedTokens
+    // getEthValue
+    function stakeTokens(uint256 _amount, address _token) public {
+        // what tokens can they stake?
+        // how much can they stake?
+    }
+}
