c92d1068289a945ed935b5ac9c6a91b153a426fc
francisco <fr.betancourt@live.com>
2022-01-25 19:36:39 -0300

    Added tokenIsAllowed code for ensuring that token can be stake.

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -7,11 +7,25 @@ contract TokenFarm {
     // issueTokens - rewards for staking
     // addAllowedTokens
     // getEthValue
+
+    address[] public allowedTokens;
+
     function stakeTokens(uint256 _amount, address _token) public {
         // what tokens can they stake?
         // how much can they stake?
         require(_amount > 0, "Amount must be more than 0");
     }
 
-    function tokenIsAllowed(address _token) public returns (bool) {}
+    function tokenIsAllowed(address _token) public returns (bool) {
+        for (
+            uint256 allowedTokensIndex = 0;
+            allowedTokensIndex < allowedTokens.length;
+            allowedTokensIndex++
+        ) {
+            if (allowedTokens[allowedTokensIndex] == _token) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
