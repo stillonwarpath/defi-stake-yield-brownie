946d0f97050fdd0125e87dff293aea73ebe36738
francisco <fr.betancourt@live.com>
2022-01-26 21:52:03 -0300

    Transfer token from user wallet to TokenFarm contract.

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -16,6 +16,7 @@ contract TokenFarm is Ownable {
     function stakeTokens(uint256 _amount, address _token) public {
         require(_amount > 0, "Amount must be more than 0");
         require(tokenIsAllowed(_token), "Token is currently no allowed");
+        IERC20(_token).transferFrom(msg.sender, address(this), _amount);
     }
 
     function addAllowedTokens(address _token) public onlyOwner {
