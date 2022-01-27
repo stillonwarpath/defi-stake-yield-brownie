8af841b64e15d7b1f5134cf2080c8189d87fa77a
francisco <fr.betancourt@live.com>
2022-01-26 22:15:05 -0300

    Added function for updating number of unique tokens an address has staked.

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -11,18 +11,34 @@ contract TokenFarm is Ownable {
     // addAllowedTokens
     // getEthValue
 
+    // 100 ETH 1:1 for every 1 ETH, we give 1 DappToken
+    // 50 ETH and 50 DAI staked, and we want to give a reward of 1 DAPP / 1 DAI
+
     // mapping token address -> staker address -> amount
     mapping(address => mapping(address => uint256)) public stakingBalance;
+    mapping(address => uint256) public uniqueTokensStaked;
+    address[] public stakers;
 
     address[] public allowedTokens;
+
+    function issueTokens() public onlyOwner {
+        // Issue tokens to all stakers
+    }
 
     function stakeTokens(uint256 _amount, address _token) public {
         require(_amount > 0, "Amount must be more than 0");
         require(tokenIsAllowed(_token), "Token is currently no allowed");
         IERC20(_token).transferFrom(msg.sender, address(this), _amount);
+        updateUniqueTokensStaked(msg.sender, _token);
         stakingBalance[_token][msg.sender] =
             stakingBalance[_token][msg.sender] +
             _amount;
+    }
+
+    function updateUniqueTokensStaked(address _user, address _token) internal {
+        if (stakingBalance[_token][_user] <= 0) {
+            uniqueTokensStaked[_user] = uniqueTokensStaked[_user] + 1;
+        }
     }
 
     function addAllowedTokens(address _token) public onlyOwner {
