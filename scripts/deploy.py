c55214486bcc79899c6fa32a3ae42876577571c9
francisco <fr.betancourt@live.com>
2022-01-31 22:15:35 -0300

    Defined function for adding allowed tokens

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -16,6 +16,12 @@ def deploy_token_farm_and_dapp_token():
     tx = dapp_token.transfer(
         token_farm.address, dapp_token.totalSupply() - KEPT_BALANCE, {"from": account}
     )
+    tx.wait(1)
+    add_allowed_tokens()
+
+
+def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
+    pass
 
 
 def main():
