29931f81bb2ae21ca14cb061fd1d44967422677d
francisco <fr.betancourt@live.com>
2022-01-31 21:52:19 -0300

    Deploy Dapp token and token farm contracts.

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,15 @@
+from scripts.helpful_scripts import get_account
+from brownie import DappToken, TokenFarm, network, config
+
+
 def deploy_token_farm_and_dapp_token():
     account = get_account()
+    dapp_token = DappToken.deploy({"from": account})
+    token_farm = TokenFarm.deploy(
+        dapp_token.address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False),
+    )
 
 
 def main():
