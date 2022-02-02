2f8b7f16d401488f7434a80704a933c58983e8e7
francisco <fr.betancourt@live.com>
2022-02-01 22:04:44 -0300

    Updated helpful_scripts.py code.

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,7 +1,31 @@
-from brownie import accounts, network, config
-import eth_utils
+from brownie import (
+    network,
+    accounts,
+    config,
+    LinkToken,
+    MockV3Aggregator,
+    MockOracle,
+    VRFCoordinatorMock,
+    Contract,
+)
+from web3 import Web3
 
-LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache"]
+NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS + [
+    "mainnet-fork",
+    "binance-fork",
+    "matic-fork",
+]
+
+contract_to_mock = {
+    "link_token": LinkToken,
+    "eth_usd_price_feed": MockV3Aggregator,
+    "vrf_coordinator": VRFCoordinatorMock,
+    "oracle": MockOracle,
+}
+
+DECIMALS = 18
+INITIAL_VALUE = Web3.toWei(2000, "ether")
 
 
 def get_account(index=None, id=None):
@@ -14,51 +38,77 @@ def get_account(index=None, id=None):
     return accounts.add(config["wallets"]["from_key"])
 
 
-# initializer=box.store, 1
-def encode_function_data(initializer=None, *args):
-    """Encodes the function call so we can work with an initializer.
-    Args:
-        initializer ([brownie.network.contract.ContractTx], optional):
-        The initializer function we want to call. Example: `box.store`.
-        Defaults to None.
-        args (Any, optional):
-        The arguments to pass to the initializer function
-    Returns:
-        [bytes]: Return the encoded bytes.
+def get_contract(contract_name):
+    """If you want to use this function, go to the brownie config and add a new entry for
+    the contract that you want to be able to 'get'. Then add an entry in the variable 'contract_to_mock'.
+    You'll see examples like the 'link_token'.
+        This script will then either:
+            - Get a address from the config
+            - Or deploy a mock to use for a network that doesn't have it
+        Args:
+            contract_name (string): This is the name that is referred to in the
+            brownie config and 'contract_to_mock' variable.
+        Returns:
+            brownie.network.contract.ProjectContract: The most recently deployed
+            Contract of the type specificed by the dictionary. This could be either
+            a mock or the 'real' contract on a live network.
     """
-    if len(args) == 0 or not initializer:
-        return eth_utils.to_bytes(hexstr="0x")
-    return initializer.encode_input(*args)
-
-
-def upgrade(
-    account,
-    proxy,
-    new_implementation_address,
-    proxy_admin_contract=None,
-    initializer=None,
-    *args
-):
-    transaction = None
-    if proxy_admin_contract:
-        if initializer:
-            encoded_function_call = encode_function_data(initializer, *args)
-            transaction = proxy_admin_contract.upgradeAndCall(
-                proxy.address,
-                new_implementation_address,
-                encoded_function_call,
-                {"from": account},
-            )
-        else:
-            transaction = proxy_admin_contract.upgrade(
-                proxy.address, new_implementation_address, {"from": account}
-            )
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
     else:
-        if initializer:
-            encoded_function_call = encode_function_data(initializer, *args)
-            transaction = proxy.upgradeToAndCall(
-                new_implementation_address, encoded_function_call, {"from": account}
+        try:
+            contract_address = config["networks"][network.show_active()][contract_name]
+            contract = Contract.from_abi(
+                contract_type._name, contract_address, contract_type.abi
             )
-        else:
-            transaction = proxy.upgradeTo(new_implementation_address, {"from": account})
-    return transaction
+        except KeyError:
+            print(
+                f"{network.show_active()} address not found, perhaps you should add it to the config or deploy mocks?"
+            )
+            print(
+                f"brownie run scripts/deploy_mocks.py --network {network.show_active()}"
+            )
+    return contract
+
+
+def fund_with_link(
+    contract_address, account=None, link_token=None, amount=1000000000000000000
+):
+    account = account if account else get_account()
+    link_token = link_token if link_token else get_contract("link_token")
+    ### Keep this line to show how it could be done without deploying a mock
+    # tx = interface.LinkTokenInterface(link_token.address).transfer(
+    #     contract_address, amount, {"from": account}
+    # )
+    tx = link_token.transfer(contract_address, amount, {"from": account})
+    print("Funded {}".format(contract_address))
+    return tx
+
+
+def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
+    """
+    Use this script if you want to deploy mocks to a testnet
+    """
+    print(f"The active network is {network.show_active()}")
+    print("Deploying Mocks...")
+    account = get_account()
+    print("Deploying Mock Link Token...")
+    link_token = LinkToken.deploy({"from": account})
+    print("Deploying Mock Price Feed...")
+    mock_price_feed = MockV3Aggregator.deploy(
+        decimals, initial_value, {"from": account}
+    )
+    print(f"Deployed to {mock_price_feed.address}")
+    print("Deploying Mock VRFCoordinator...")
+    mock_vrf_coordinator = VRFCoordinatorMock.deploy(
+        link_token.address, {"from": account}
+    )
+    print(f"Deployed to {mock_vrf_coordinator.address}")
+
+    print("Deploying Mock Oracle...")
+    mock_oracle = MockOracle.deploy(link_token.address, {"from": account})
+    print(f"Deployed to {mock_oracle.address}")
+    print("Mocks Deployed!")
