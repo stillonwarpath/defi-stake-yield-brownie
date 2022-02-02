704cb8c07db00c814850b1e9f867c9ae9fa433d5
francisco <fr.betancourt@live.com>
2022-02-01 22:49:23 -0300

    Add allowed tokens and set their price feed.

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -29,7 +29,14 @@ def deploy_token_farm_and_dapp_token():
 
 
 def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
-    pass
+    for token in dict_of_allowed_tokens:
+        add_tx = token_farm.addAllowedTokens(token.address, {"from": account})
+        add_tx.wait(1)
+        set_tx = token_farm.setPriceFeedContract(
+            token.address, dict_of_allowed_tokens[token], {"from": account}
+        )
+        set_tx.wait(1)
+    return token_farm
 
 
 def main():
