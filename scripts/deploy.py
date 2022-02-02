5e4ecf3f7f8f79cef4689ee8850dca464a7fa8cf
francisco <fr.betancourt@live.com>
2022-02-01 22:45:14 -0300

    Get contracts of price feeds for dapp, fau and weth tokens.

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -20,7 +20,12 @@ def deploy_token_farm_and_dapp_token():
     # dapp_token, weth_token, fau_token/dai
     weth_token = get_contract("weth_token")
     fau_token = get_contract("fau_token")
-    add_allowed_tokens()
+    dict_of_allowed_tokens = {
+        dapp_token: get_contract("dai_usd_price_feed"),
+        fau_token: get_contract("dai_usd_price_feed"),
+        weth_token: get_contract("eth_usd_price_feed"),
+    }
+    add_allowed_tokens(token_farm, dict_of_allowed_tokens, account)
 
 
 def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
