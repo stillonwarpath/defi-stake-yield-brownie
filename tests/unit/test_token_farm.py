8eefe2746bab75d94da9eaaf3bc55e81eafeb300
francisco <fr.betancourt@live.com>
2022-02-03 11:42:03 -0300

    Get account and a non owner account for testing owner functions.

--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -1,9 +1,13 @@
 from brownie import network
 import pytest
 from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS
+from scripts.deploy import deploy_token_farm_and_dapp_token
 
 
 def test_set_price_feed_contract():
     # Arrange
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip("Only for local testing!")
+    account = get_account()
+    non_owner = get_account(index=1)
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
