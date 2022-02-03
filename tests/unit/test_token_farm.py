a795edfc88d315cc1d546d77a8b955623a344f8c
francisco <fr.betancourt@live.com>
2022-02-03 11:48:17 -0300

    Called tokenPriceFeedMapping for checking that token price feed was set

--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -16,6 +16,9 @@ def test_set_price_feed_contract():
     non_owner = get_account(index=1)
     token_farm, dapp_token = deploy_token_farm_and_dapp_token()
     # Act
+    price_feed_address = get_contract("eth_usd_price_feed")
     token_farm.setPriceFeedContract(
-        dapp_token.address, get_contract("eth_usd_price_feed"), {"from": account}
+        dapp_token.address, price_feed_address, {"from": account}
     )
+    # Assert
+    assert token_farm.tokenPriceFeedMapping(dapp_token.address) == price_feed_address
