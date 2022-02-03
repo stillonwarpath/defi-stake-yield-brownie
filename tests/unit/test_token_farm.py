846ff77a1067ad24b0473357dea03762330b5027
francisco <fr.betancourt@live.com>
2022-02-03 11:44:42 -0300

    Called function for setting price feed for testing price feed.

--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -1,6 +1,10 @@
 from brownie import network
 import pytest
-from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    get_account,
+    get_contract,
+)
 from scripts.deploy import deploy_token_farm_and_dapp_token
 
 
@@ -11,3 +15,7 @@ def test_set_price_feed_contract():
     account = get_account()
     non_owner = get_account(index=1)
     token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    # Act
+    token_farm.setPriceFeedContract(
+        dapp_token.address, get_contract("eth_usd_price_feed"), {"from": account}
+    )
