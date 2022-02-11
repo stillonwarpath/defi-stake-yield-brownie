c48368e7a6ce5a1707e0dd65fbae5753e79f743c
francisco <fr.betancourt@live.com>
2022-02-11 14:07:50 -0300

    Added button to site for connect and disconnect metamask wallet. Added script for copying brownie config file to json.

---
+++ front_end/src/components/Main.tsx
@@ -0,0 +1,9 @@
+export const Main = () => {
+    // Show token values from the wallet
+
+    // Get the address of different tokens
+    // Get the balance of the users wallet
+
+    // send the brownie-config to our ``src` folder
+    // send the build folder
+}
