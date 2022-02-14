4fbc08b27572ad8cdac95f301395bcc5feaf9892
francisco <fr.betancourt@live.com>
2022-02-14 12:07:04 -0300

    Removed Rinkeby from supported chains for simplicity

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -7,7 +7,7 @@ import { Main } from './components/Main'
 function App() {
   return (
     <DAppProvider config={{
-      supportedChains: [ChainId.Kovan, ChainId.Rinkeby]
+      supportedChains: [ChainId.Kovan]
     }}>
       <Header />
       <Container maxWidth="md">
