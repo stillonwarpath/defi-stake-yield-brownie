5606f7df6f225ea0173e1391627ded33af3e1de1
francisco <fr.betancourt@live.com>
2022-02-14 12:34:41 -0300

    Added supported tokens information

--- front_end/src/components/Main.tsx
+++ front_end/src/components/Main.tsx
@@ -3,6 +3,17 @@ import helperConfig from '../helper-config.json'
 import networkMapping from '../chain-info/deployments/map.json'
 import { constants } from 'ethers'
 import brownieConfig from '../brownie-config.json'
+import dapp from '../dapp.png'
+import eth from '../eth.png'
+import dai from '../dai.png'
+
+
+
+export type Token = {
+    image: string
+    address: string
+    name: string
+}
 
 export const Main = () => {
     // Show token values from the wallet
@@ -18,5 +29,24 @@ export const Main = () => {
     const dappTokenAddress = chainId ? networkMapping[String(chainId)]["DappToken"][0] : constants.AddressZero
     const wethTokenAddress = chainId ? brownieConfig["networks"][networkName]["weth_token"] : constants.AddressZero
     const fauTokenAddress = chainId ? brownieConfig["networks"][networkName]["fau_token"] : constants.AddressZero
+
+    const supportedTokens: Array<Token> = [
+        {
+            image: dapp,
+            address: dappTokenAddress,
+            name: "DAPP"
+        },
+        {
+            image: eth,
+            address: wethTokenAddress,
+            name: "WETH"
+        },
+        {
+            image: dai,
+            address: fauTokenAddress,
+            name: "DAI"
+        }
+    ]
+
     return (<div>Hi</div>)
 }
