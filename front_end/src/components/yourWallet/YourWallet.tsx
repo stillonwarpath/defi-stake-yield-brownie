e84307728173229d7a75e9d8ec4c1b755441a5c1
francisco <fr.betancourt@live.com>
2022-02-16 12:40:07 -0300

    Added tabs for selecting tokens inside user wallet

--- front_end/src/components/yourWallet/YourWallet.tsx
+++ front_end/src/components/yourWallet/YourWallet.tsx
@@ -1,9 +1,38 @@
 import { Token } from '../Main'
+import React, { useState } from 'react'
+import { Box } from '@material-ui/core'
+import { TabContext, TabList, TabPanel } from '@material-ui/lab'
+import { Tab } from '@material-ui/core'
 
 interface YourWalletProps {
     supportedTokens: Array<Token>
 }
 
 export const YourWallet = ({ supportedTokens }: YourWalletProps) => {
-  return (<div>I'm your wallet</div>)
+
+  const [selectedTokenIndex, setSelectedTokenIndex] = useState<number>(0)
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+    setSelectedTokenIndex(parseInt(newValue))
+  }
+
+  return (
+    <Box>
+      <h1>Your wallet</h1>
+      <Box>
+        <TabContext value={selectedTokenIndex.toString()}>
+          <TabList onChange={handleChange} aria-label="stake form tabs">
+            {supportedTokens.map((token, index) => {
+              return (
+                <Tab label={token.name}
+                    value={index.toString()}
+                    key={index}>
+                </Tab>
+              )
+            })}
+          </TabList>
+        </TabContext>
+      </Box>
+    </Box>
+  )
 }
