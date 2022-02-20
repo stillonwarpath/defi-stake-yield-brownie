aa425ad50f7aac3b40fba3069f8b95e48757f063
francisco <fr.betancourt@live.com>
2022-02-20 13:16:49 -0300

    Get ERC20 contract of token address

--- front_end/src/hooks/useStakeTokens.ts
+++ front_end/src/hooks/useStakeTokens.ts
@@ -3,6 +3,7 @@ import { constants, utils } from 'ethers';
 import { Contract } from '@ethersproject/contracts'
 import TokenFarm from '../chain-info/contracts/TokenFarm.json'
 import networkMapping  from '../chain-info/deployments/map.json'
+import ERC20 from '../chain-info/contracts/MockERC20.json'
 
 export const useStakeTokens = (tokenAddress: string) => {
     // address
@@ -14,6 +15,9 @@ export const useStakeTokens = (tokenAddress: string) => {
     const tokenFarmInterface = new utils.Interface(abi)
     const tokenFarmContract = new Contract( tokenFarmAddress, tokenFarmInterface )
 
+    const ERC20ABI = ERC20.abi
+    const erc20Interface = new utils.Interface(ERC20ABI)
+    const erc20Contract = new Contract(tokenAddress, erc20Interface)
     // approve
     // stake tokens
 }
