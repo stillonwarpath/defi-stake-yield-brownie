49a0653366adcc702e2d9ea553846922d28df99a
francisco <fr.betancourt@live.com>
2022-02-22 16:32:45 -0300

    used contract function stake send

--- front_end/src/hooks/useStakeTokens.ts
+++ front_end/src/hooks/useStakeTokens.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useEthers, useContractFunction } from '@usedapp/core'
 import { constants, utils } from 'ethers';
 import { Contract } from '@ethersproject/contracts'
@@ -19,8 +19,8 @@ export const useStakeTokens = (tokenAddress: string) => {
     const ERC20ABI = ERC20.abi
     const erc20Interface = new utils.Interface(ERC20ABI)
     const erc20Contract = new Contract(tokenAddress, erc20Interface)
+
     // approve
-    // stake tokens
     const { send: approveErc20Send, state: approveErc20State } = 
     useContractFunction(erc20Contract, "approve", { 
         transactionName: 'Approve ERC20 transfer' 
@@ -28,5 +28,18 @@ export const useStakeTokens = (tokenAddress: string) => {
     const approve = (amount: string) => {
         return approveErc20Send(tokenFarmAddress, amount)
     }
+
+    // stake tokens
+    const { send: stakeSend, state: stakeState } = useContractFunction(tokenFarmContract, "stakeTokens", {
+        transactionName: 'Stake Tokens'
+    })
+
+    //useEffect
+    useEffect(() => {
+        if (approveErc20State.status === 'Success') {
+        
+        }
+    }, [approveErc20State])
+
     return { approve, approveErc20State }
 }
